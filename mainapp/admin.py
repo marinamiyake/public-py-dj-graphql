d9fef87817cadab2b6ceebf61af31dbb2642ac4a
Marina Miyake <marinamiyake@outlook.com>
2023-08-09 23:13:50 +0100

    Add new model (Moment)

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp import models
+
+
+class MomentModelAdmin(admin.ModelAdmin):
+    disp_fields = [
+        "id",
+        "title",
+        "city_name",
+        "weather_description",
+        "temp",
+        "temp_min",
+        "temp_max",
+        "humidity",
+        "comment",
+        "created_at",
+    ]
+    save_on_top = True
+    save_as = True
+    list_display = disp_fields
+    date_hierarchy = "created_at"
+    search_fields = ('id', 'title')
+    sortable_by = disp_fields
+    readonly_fields = [
+        'created_at',
+    ]
+
+
+admin.site.register(models.Moment, MomentModelAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Moment(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    city_name = models.CharField(max_length=50, blank=True, null=True)
+    weather_description = models.CharField(max_length=100, blank=True, null=True)
+    temp = models.FloatField()
+    temp_min = models.FloatField()
+    temp_max = models.FloatField()
+    humidity = models.FloatField()
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Moment ID: " + str(self.id)
